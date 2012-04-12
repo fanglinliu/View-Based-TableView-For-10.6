0ca898d3173ba78474897db0c05cc07aaba118a7
fanglinliu <fanglinliu@gmail.com>
2012-04-12 17:24:26 +0800

    Initial Commit

---
+++ View-Based-TableView-For-10/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  View-Based-TableView-For-10
+//
+//  Created by fanglin liu on 12-4-12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ View-Based-TableView-For-10/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  View-Based-TableView-For-10
+//
+//  Created by fanglin liu on 12-4-12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
